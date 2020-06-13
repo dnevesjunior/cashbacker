1da7e330f11f6f8f459d25e42159f5caef8c7f8b
denisfroes <dneves.junior@gmail.com>
2020-06-13 14:36:53 -0300

    feat: model validations

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,2 +1,6 @@
 class Offer < ApplicationRecord
+  belongs_to :advertiser
+  validates :advertiser, :url, :description, :starts_at, presence: true
+  validates :url, format: URI::regexp(%w[http https])
+  validates :description, length: { maximum: 500 }
 end
